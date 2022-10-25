5af76b67d99d6c5dece8cd647f093ac53a7504c9
dani <danieljuhe@gmail.com>
2022-10-25 19:44:40 +0200

    changed package.json host

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,24 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name===null?"":variables.name}</h1>
-          <h2>${variables.lastname===null?"":variables.lastname}</h2>
-          <h3>${variables.city===null?"":variables.city}, ${variables.country===null?"":variables.country}</h3>
+          <h1>${variables.name === null ? "" : variables.name}</h1>
+          <h2>${variables.lastname === null ? "" : variables.lastname}</h2>
+          <h3>${variables.city === null ? "" : variables.city}, ${
+    variables.country === null ? "" : variables.country
+  }</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}y"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin}y"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }y"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }y"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
